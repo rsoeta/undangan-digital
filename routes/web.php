47a2211525b49d480460bb1b0c423fb57eb4700c
Rian Sutarsa <riansoetarsa@outlook.com>
2026-09-12 01:32:40 +0700

    Update logo and fix local route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvitationController;
 
+Route::get('/', function () {
+    return redirect('/admin'); // Atau arahkan ke tampilan/slug default
+});
+
 // Route untuk submit RSVP menggunakan metode POST
 Route::post('/{slug}/rsvp', [InvitationController::class, 'storeRsvp'])->name('rsvp.store');
 
